c8b2403a4b01f216862cbcefa026f0ec2a23b91c
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-09-20 14:40:10 +0900

    Update intent utils layout

--- app/src/main/java/com/kwang0/intent/utils/IntentUtils.kt
+++ app/src/main/java/com/kwang0/intent/utils/IntentUtils.kt
@@ -1,4 +1,34 @@
 package com.kwang0.intent.utils
 
-class IntentUtils {
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+
+object IntentUtils {
+    fun startExternalBrowser(context: Context, url: String?) {
+        url?.also {
+            try {
+                val intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse(it))
+                context.startActivity(intent)
+            } catch (e: Exception) { }
+        }
+    }
+
+    fun startEmail(context: Context, email: String?) {
+        email?.also {
+            try {
+                val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:$email"))
+                context.startActivity(intent)
+            } catch (e: Exception) { }
+        }
+    }
+
+    fun startPhoneCall(context: Context, phone: String?) {
+        phone?.also {
+            try {
+                val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phone"))
+                context.startActivity(intent)
+            } catch (e: Exception) { }
+        }
+    }
 }
