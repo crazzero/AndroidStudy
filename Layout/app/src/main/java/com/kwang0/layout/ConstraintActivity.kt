2dc8c47e80b9456d0c2dc22ad8b25723499a098f
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-10-01 00:04:27 +0900

    Fix main activity to constraint activity

--- Layout/app/src/main/java/com/kwang0/layout/ConstraintActivity.kt
+++ Layout/app/src/main/java/com/kwang0/layout/ConstraintActivity.kt
@@ -3,9 +3,9 @@ package com.kwang0.layout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ConstraintActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_constraint)
     }
 }
