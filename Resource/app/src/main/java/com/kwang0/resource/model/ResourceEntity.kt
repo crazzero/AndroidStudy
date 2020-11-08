c1d3f731f7312e5a7740de40a18f1a4898574413
kwang0 <cky1203@gmail.com>
2020-11-08 18:54:41 +0900

    Fix resource enum to class

--- Resource/app/src/main/java/com/kwang0/resource/model/ResourceEntity.kt
+++ Resource/app/src/main/java/com/kwang0/resource/model/ResourceEntity.kt
@@ -1,15 +1,15 @@
 package com.kwang0.resource.model
 
-interface ResourceEntity {
-    val title: String
-    val type: Int
-    val key: String
-    val value: String
+class ResourceEntity(
+    val title: String,
+    val type: Int,
+    val payload: Any
+) {
 
     companion object {
-        const val ANIMATION_TYPE = 0
+        const val STRING_TYPE = 0
         const val COLOR_TYPE = 1
-        const val STRING_TYPE = 2
-        const val DRAWABLE_TYPE = 3
+        const val DRAWABLE_TYPE = 2
+        const val ANIMATION_TYPE = 3
     }
 }
