bf96e291e1873795028138f476d904f93b4677d0
kwang0 <cky1203@gmail.com>
2021-01-16 22:58:17 +0900

    Fix flex box activity to navigation activity

--- Layout/app/src/main/java/com/kwang0/layout/ui/FlexActivity.kt
+++ Layout/app/src/main/java/com/kwang0/layout/ui/FlexActivity.kt
@@ -1,4 +1,14 @@
 package com.kwang0.layout.ui
 
-class FlexActivity {
+import com.kwang0.layout.R
+import com.kwang0.layout.ui.navigation.NavigationActivity
+
+class FlexActivity : NavigationActivity() {
+
+    override val layoutId: Int
+        get() = R.layout.activity_flex
+
+    override fun initActivity() {
+        TODO("Not yet implemented")
+    }
 }
