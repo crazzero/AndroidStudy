31d9f9a444606f8282b91db665a5d653456b123c
kwang0 <cky1203@gmail.com>
2021-01-22 00:16:39 +0900

    Add squircle smooth corner project

---
+++ SquircleSmoothCorner/app/src/main/java/com/kwang0/squircle_smooth_corner/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kwang0.squircle_smooth_corner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
