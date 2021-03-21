7439799d1ec371b032ff68ee8a76e691304e9b21
kwang0 <cky1203@gmail.com>
2021-03-21 21:57:20 +0900

    Fix action layout, style type

--- AndroidWYSIWYG/app/src/main/java/com/kwang0/androidwisywig/ui/layout/ActionLayout.kt
+++ AndroidWYSIWYG/app/src/main/java/com/kwang0/androidwisywig/ui/layout/ActionLayout.kt
@@ -5,12 +5,24 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
 import com.kwang0.androidwisywig.R
+import com.kwang0.androidwisywig.enumerate.StyleType
+import com.kwang0.androidwisywig.utils.ResourceUtils
+import kotlinx.android.synthetic.main.layout_action.view.*
 
 class ActionLayout @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
+    private var styleType = StyleType.BASE
+
     init {
         LayoutInflater.from(context).inflate(R.layout.layout_action, this)
+
+        cardActionBold.setOnClickListener {
+            if (styleType != StyleType.BOLD) {
+                styleType = StyleType.BOLD
+                cardActionBold.setCardBackgroundColor(ResourceUtils.getColor(R.color.black10))
+            }
+        }
     }
 }
