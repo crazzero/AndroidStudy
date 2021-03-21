8ff49853684baee51b61c52ceecf5f6007092a88
kwang0 <cky1203@gmail.com>
2021-03-21 20:55:29 +0900

    Remove action type, add style type

--- AndroidWYSIWYG/app/src/main/java/com/kwang0/androidwisywig/ui/layout/ActionLayout.kt
+++ AndroidWYSIWYG/app/src/main/java/com/kwang0/androidwisywig/ui/layout/ActionLayout.kt
@@ -5,7 +5,6 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
 import com.kwang0.androidwisywig.R
-import com.kwang0.androidwisywig.enumerate.ActionType
 
 class ActionLayout @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -13,9 +12,5 @@ class ActionLayout @JvmOverloads constructor(
 
     init {
         LayoutInflater.from(context).inflate(R.layout.layout_action, this)
-    }
-
-    fun setActions(actionTypes: List<ActionType>) {
-        
     }
 }
