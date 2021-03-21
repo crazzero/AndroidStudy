6fb893259f1af300dec3535aed82ed5b0cd204f5
kwang0 <cky1203@gmail.com>
2021-03-21 17:00:16 +0900

    Apply get drawable in resource utils

--- AndroidWYSIWYG/app/src/main/java/com/kwang0/androidwisywig/utils/ResourceUtils.kt
+++ AndroidWYSIWYG/app/src/main/java/com/kwang0/androidwisywig/utils/ResourceUtils.kt
@@ -1,4 +1,24 @@
 package com.kwang0.androidwisywig.utils
 
-class ResourceUtils {
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
+import android.os.Build
+import com.kwang0.androidwisywig.App
+
+object ResourceUtils {
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    fun getDrawable(icon: Int): Drawable? {
+        try {
+            App.appContext?.also {
+                return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    it.resources.getDrawable(icon, null)
+                } else {
+                    it.resources.getDrawable(icon)
+                }
+            }
+        } catch (e: Exception) { }
+
+        return null
+    }
 }
