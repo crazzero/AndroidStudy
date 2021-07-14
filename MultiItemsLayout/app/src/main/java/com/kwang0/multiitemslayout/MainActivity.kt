42d1c13cde5ad30d93918eec155dc5e7a42c30af
kwang0e <cky1203@gmail.com>
2021-07-14 21:29:58 +0900

    Apply main activity layout binding

--- MultiItemsLayout/app/src/main/java/com/kwang0/multiitemslayout/MainActivity.kt
+++ MultiItemsLayout/app/src/main/java/com/kwang0/multiitemslayout/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.kwang0.multiitemslayout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import com.kwang0.multiitemslayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(LayoutInflater.from(this))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
