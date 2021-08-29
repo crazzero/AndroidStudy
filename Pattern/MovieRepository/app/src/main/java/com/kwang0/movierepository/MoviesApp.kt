4ed6f869a10e2448589b0ace060b71f537ca46af
kwang0e <cky1203@gmail.com>
2021-08-29 09:55:16 +0900

    Add app context for movies app

--- Pattern/MovieRepository/app/src/main/java/com/kwang0/movierepository/MoviesApp.kt
+++ Pattern/MovieRepository/app/src/main/java/com/kwang0/movierepository/MoviesApp.kt
@@ -6,10 +6,20 @@ import dagger.android.DaggerApplication
 
 class MoviesApp: DaggerApplication() {
 
+    override fun onCreate() {
+        super.onCreate()
+
+        appContext = this
+    }
+
     override fun applicationInjector(): AndroidInjector<MoviesApp> {
         val component = DaggerAppComponent.builder().application(this).build();
         component.inject(this);
         return component
     }
 
+    companion object {
+        @JvmStatic
+        lateinit var appContext: MoviesApp
+    }
 }
