a2a6171b18f51132c954165ccb76cdef67fba419
kwang0e <cky1203@gmail.com>
2021-08-29 10:17:09 +0900

    Fix git ignore for key

--- Pattern/MovieRepository/app/src/main/java/com/kwang0/movierepository/data/source/remote/MoviesRemoteDataSource.kt
+++ Pattern/MovieRepository/app/src/main/java/com/kwang0/movierepository/data/source/remote/MoviesRemoteDataSource.kt
@@ -1,8 +1,10 @@
 package com.kwang0.movierepository.data.source.remote
 
+import com.kwang0.movierepository.R
 import com.kwang0.movierepository.data.entity.MovieEntity
 import com.kwang0.movierepository.data.exception.NetworkConnectionException
 import com.kwang0.movierepository.data.exception.ServiceException
+import com.kwang0.movierepository.utils.ResUtils
 import java.io.IOException
 
 
@@ -18,7 +20,7 @@ class MoviesRemoteDataSourceImpl(private val movieService: MovieService): Movies
     override fun getMovies(): List<MovieEntity> {
         try {
             // You can get api key at (https://developers.themoviedb.org/3/getting-started)
-            val response = movieService.getMovies("").execute()
+            val response = movieService.getMovies(ResUtils.getString("API_KEY")).execute()
             if(response.isSuccessful){
                 return response.body()?.movies ?: emptyList()
             } else{
