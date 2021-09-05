fd4319a2ef725e961ba8cdd0151530ab4414f493
kwang0e <cky1203@gmail.com>
2021-09-05 16:26:44 +0900

    Add commit, back stack comments

--- Fragment/app/src/main/java/com/kwang0/fragment/MainActivity.kt
+++ Fragment/app/src/main/java/com/kwang0/fragment/MainActivity.kt
@@ -14,9 +14,31 @@ class MainActivity : AppCompatActivity() {
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, FirstFragment.newInstance())
-                .commitNow()
+
+            // commit 비동기처리 && save state 에 자유롭지 않음
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, FirstFragment.newInstance())
+//                .commit()
+            // commit 동기처리 && save state 에 자유롭지 않음
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, FirstFragment.newInstance())
+//                .commitNow()
+            // commit 비동기처리 && save state 에 자유로움
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, FirstFragment.newInstance())
+//                .commitAllowingStateLoss()
+            // commit 동기처리 && save state 에 자유로움
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, FirstFragment.newInstance())
+//                .commitNowAllowingStateLoss()
+
+            // commit 비동기처리 된 친구들을 바로 실행시킬 때 사용
+//            supportFragmentManager.executePendingTransactions()
+
+            // stack 에 있는 fragment 을 비동기로 pop 시킴
+//            supportFragmentManager.popBackStack()
+            // stack 에 있는 fragment 을 동기로 pop 시킴
+//            supportFragmentManager.popBackStack()
         }
     }
 }
