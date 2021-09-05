91141a043ecdff698d915f1280c47a240db8dd9d
kwang0e <cky1203@gmail.com>
2021-09-05 15:30:11 +0900

    Fix second, third fragment

--- Fragment/app/src/main/java/com/kwang0/fragment/ui/third/ThirdFragment.kt
+++ Fragment/app/src/main/java/com/kwang0/fragment/ui/third/ThirdFragment.kt
@@ -7,16 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.kwang0.fragment.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ThirdFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ThirdFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -34,21 +24,14 @@ class ThirdFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.third_fragment, container, false)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ThirdFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
+
+        private const val ARG_PARAM1 = "param1"
+        private const val ARG_PARAM2 = "param2"
+
         fun newInstance(param1: String, param2: String) =
             ThirdFragment().apply {
                 arguments = Bundle().apply {
