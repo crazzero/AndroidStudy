4ec78a209e9cd8463af7a3a308d7ba595b4513c5
kwang0e <cky1203@gmail.com>
2021-09-12 22:18:34 +0900

    Apply view model for second fragment

--- Fragment/app/src/main/java/com/kwang0/fragment/ui/second/SecondFragment.kt
+++ Fragment/app/src/main/java/com/kwang0/fragment/ui/second/SecondFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.kwang0.fragment.R
+import androidx.fragment.app.viewModels
 import com.kwang0.fragment.databinding.SecondFragmentBinding
 
 class SecondFragment : Fragment() {
@@ -14,6 +14,7 @@ class SecondFragment : Fragment() {
     private var param2: String? = null
     private var _binding: SecondFragmentBinding? = null
     private val binding get() = _binding!!
+    private val viewModel by viewModels<SecondViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +30,15 @@ class SecondFragment : Fragment() {
     ): View? {
         _binding = SecondFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textSecondFragment.setOnClickListener {
+            viewModel.updateFragmentCount
+        }
+        binding.textSecondFragment.text = "${viewModel.updateFragmentCount.value}"
     }
 
     override fun onDestroyView() {
--- Fragment/app/src/main/java/com/kwang0/fragment/ui/second/SecondViewModel.kt
+++ Fragment/app/src/main/java/com/kwang0/fragment/ui/second/SecondViewModel.kt
@@ -1,7 +1,15 @@
 package com.kwang0.fragment.ui.second
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SecondViewModel : ViewModel() {
 
+    private val _updateFragmentCount = MutableLiveData<Int>(0)
+    val updateFragmentCount: LiveData<Int> get() = _updateFragmentCount
+
+    fun insertCountTwo() {
+        _updateFragmentCount.value = (updateFragmentCount.value ?: 0) + 2
+    }
 }
