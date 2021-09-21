70d16d2a24c86fb5ac712fd12bb5f0c62a630d5d
kwang0e <cky1203@gmail.com>
2021-09-21 09:54:10 +0900

    Add sample project for compose sample

---
+++ Compose/app/src/main/java/com/kwang0/compose/ui/sample/Artist.kt
@@ -0,0 +1,13 @@
+package com.kwang0.compose.ui.sample
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ArtistCard() {
+    Column {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
+    }
+}
