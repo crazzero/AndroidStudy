89b41e751f0dbcf74b64cc73993311f43cfae20f
kwang0e <cky1203@gmail.com>
2021-09-22 20:32:21 +0900

    Fix main adapter, view holder

--- DI/app/src/main/java/com/kwang0/di/feature/MainAdapter.kt
+++ DI/app/src/main/java/com/kwang0/di/feature/MainAdapter.kt
@@ -1,4 +1,26 @@
 package com.kwang0.di.feature
 
-class MainAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MainAdapter :
+    RecyclerView.Adapter<MainViewHolder>() {
+
+    private var items: List<String> = arrayListOf()
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): MainViewHolder {
+        if (viewGroup is RecyclerView) {
+            return MainViewHolder(viewGroup)
+        } else {
+            throw RuntimeException("Not bound to RecyclerView")
+        }
+    }
+
+    override fun onBindViewHolder(mainViewHolder: MainViewHolder, position: Int) {
+        mainViewHolder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.count()
+    }
 }
--- DI/app/src/main/java/com/kwang0/di/feature/MainViewHolder.kt
+++ DI/app/src/main/java/com/kwang0/di/feature/MainViewHolder.kt
@@ -1,4 +1,19 @@
 package com.kwang0.di.feature
 
-class MainViewHolder {
+import android.view.LayoutInflater
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.kwang0.di.databinding.HolderMainBinding
+
+class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private var binding: HolderMainBinding? = null
+
+    init {
+        binding = HolderMainBinding.inflate(LayoutInflater.from(itemView.context), null, false)
+    }
+
+    fun bind(text: String) {
+        binding?.textMainHolder?.text = text
+    }
 }
