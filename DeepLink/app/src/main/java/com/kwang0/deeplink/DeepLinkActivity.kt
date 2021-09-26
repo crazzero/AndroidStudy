fb93b26872bd3e896d07f4dadafaa35891d6e017
kwang0e <cky1203@gmail.com>
2021-09-26 15:19:54 +0900

    Add deep link activity

---
+++ DeepLink/app/src/main/java/com/kwang0/deeplink/DeepLinkActivity.kt
@@ -0,0 +1,10 @@
+package com.kwang0.deeplink
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DeepLinkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
