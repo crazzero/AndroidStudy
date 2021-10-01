6d8a46d9e5b080ab98ad3081d841ab5d01853d46
kwang0e <cky1203@gmail.com>
2021-10-01 23:29:48 +0900

    Optimize import for view holder contract

--- DI/app/src/main/java/com/kwang0/di/feature/contract/MainViewHolderContract.kt
+++ DI/app/src/main/java/com/kwang0/di/feature/contract/MainViewHolderContract.kt
@@ -1,9 +1,5 @@
 package com.kwang0.di.feature.contract
 
-import com.kwang0.di.feature.di.ViewHolderComponent
-import dagger.hilt.EntryPoint
-import dagger.hilt.InstallIn
-
 interface MainViewHolderContract {
 
     interface ViewHolder {
