092315cde82180483a497cb5ba5d7718ef8179ee
kwang0e <cky1203@gmail.com>
2021-10-02 18:50:44 +0900

    Add move deep link page to main

--- DeepLink/app/src/main/java/com/kwang0/deeplink/DeepLinkActivity.kt
+++ DeepLink/app/src/main/java/com/kwang0/deeplink/DeepLinkActivity.kt
@@ -1,10 +1,17 @@
 package com.kwang0.deeplink
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class DeepLinkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Intent(this, MainActivity::class.java)
+            .also {
+                it.putExtra("Deep Link", "worked!!")
+                startActivity(it)
+            }
     }
 }
