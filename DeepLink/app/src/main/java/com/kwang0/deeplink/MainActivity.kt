ca674a423e70887c5eb8f530dd05dc857f515ef9
kwang0e <cky1203@gmail.com>
2021-10-02 18:54:30 +0900

    Add view binding for main activity

--- DeepLink/app/src/main/java/com/kwang0/deeplink/MainActivity.kt
+++ DeepLink/app/src/main/java/com/kwang0/deeplink/MainActivity.kt
@@ -2,10 +2,14 @@ package com.kwang0.deeplink
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kwang0.deeplink.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater, null, false)
+        setContentView(binding.root)
     }
 }
