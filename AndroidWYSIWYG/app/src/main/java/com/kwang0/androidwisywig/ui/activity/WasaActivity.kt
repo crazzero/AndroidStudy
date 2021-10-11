8521e96d0491949944793e15d693f381fe43d54e
kwang0e <cky1203@gmail.com>
2021-10-11 21:25:09 +0900

    Add wasa activity

---
+++ AndroidWYSIWYG/app/src/main/java/com/kwang0/androidwisywig/ui/activity/WasaActivity.kt
@@ -0,0 +1,12 @@
+package com.kwang0.androidwisywig.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kwang0.androidwisywig.R
+
+class WasaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wasa)
+    }
+}
